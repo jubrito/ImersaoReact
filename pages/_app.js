bd620d4f534ce58f8d276bb2482b03e1a639975c
Juliana <jubrito@usp.br>
2021-01-25 20:59:42 -0300

    Next.JS, Create React App, Styled Components e primeiro deploy na Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,29 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from "../db.json";
 
 const GlobalStyle = createGlobalStyle`
+  // Reset
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato, sans-serif';
+    // Branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle /> {/* GlobalStyle dentro do theme para que o db json seja lido em todos os componentes*/}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
 import styled from 'styled-components'
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import QuizBackGround from "../src/components/QuizBackground";
+import GitHubCorner from "../src/components/GitHubCorner";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// Sem o Styled-Components
+// function Title(props) {
+//   return (
+//     // .children: podemos ter um ou mais elementos filhos carregando dentro da tag
+//     <h1>{props.children}</h1>
+//   )
+// }
+// Com o Styled-Components conseguimos criar uma variável passando o style de maneira dinâmica
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+//   background-color: ${({ theme }) => {
+//   return theme.colors.primary
+// }};
+// `
+
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    // Ao invés de fazer assim abaixo, criamos o componente com o style do background
+    // <div style={{ backgroundImage: `url (${db.bg})` }}>
+    <QuizBackGround backgroundImage={db.backgroundImage}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Experiência do Usuário</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner/>
+    </QuizBackGround>
+  )
 }
