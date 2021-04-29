e8a535dc4d1e569c07e8c3bf8aa722638858d6c9
Juliana <jubrito@usp.br>
2021-04-29 00:58:56 -0300

    teste oi

--- pages/Quiz/index.js
+++ pages/Quiz/index.js
@@ -5,13 +5,14 @@ import QuizScreen from '../../src/screens/Quiz';
 import db from '../../db.json';
 export default function QuizDaGaleraPage() {
   return (
-    <ThemeProvider theme={db.theme}>
-      <QuizScreen
-        externalQuestions={db.questions}
-        externalBg={db.bg}
-        externalBgMobile={db.bg_mobile}
-        externalTextResults={db.results}
-      />
-    </ThemeProvider>
+    <p>oi</p>
+    // <ThemeProvider theme={db.theme}>
+    //   <QuizScreen
+    //     externalQuestions={db.questions}
+    //     externalBg={db.bg}
+    //     externalBgMobile={db.bg_mobile}
+    //     externalTextResults={db.results}
+    //   />
+    // </ThemeProvider>
   );
 }
